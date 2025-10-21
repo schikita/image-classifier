5ef14c79b26383c282ecc85672cc4bfd86853466
WnayM <s30221@pjwstk.edu.pl>
2025-10-21 20:21:02 +0200

    Completed ai/model and ai/train

--- ai/model.py
+++ ai/model.py
@@ -1,8 +1,8 @@
-import tensorflow as tf
+import tensorflow as tf 
 from pathlib import Path
 
-class KNNModel:
-    def __init__(self, img_size = (128,128), num_classes = 2, learning_rate = 0.001):
+class CNNModel:
+    def __init__(self, img_size = (128,128), num_classes = 3, learning_rate = 0.001):
         self.img_size = img_size
         self.num_classes = num_classes
         self.learning_rate = learning_rate
@@ -22,9 +22,28 @@ class KNNModel:
         
         model.compile(
             optimizer = tf.keras.optimizers.Adam(self.learning_rate),
-            loss = 'categorial_crossentropy',
+            loss = 'categorcial_crossentropy',
             metricd = ['accuracy']
         )
 
         return model
     
+    def train(self, train_flow,val_flow, epochs=10,save_path = "model/model.h5"):
+        Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+        callbacks = [
+            tf.keras.callbacks.ModelCheckpoint(save_path, save_best_only = True, monitor = 'val_accuracy'),
+            tf.keras.callbacks.EarlyStopping(patience = 3, restore_best_weights = True)
+        ]
+        history = self.model.fit(train_flow, validation_data = val_flow, epochs = epochs, callbacks = callbacks)
+        return history
+    
+    def save(self, path = 'model/model.h5'):
+        Path(path).parent.mkdir(parents=True,exist_ok=True)
+        self.model.save(path)
+
+    def load(self, path = 'model/model.h5'):
+        self.model = tf.keras.models.load_model(path)
+        return self
+    
+    def predict(self, img):
+        return self.model.predict(img)
