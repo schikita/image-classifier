346b9847526f35ed00c0b180897ac94ed3af6e93
WnayM <s30221@pjwstk.edu.pl>
2025-11-02 12:41:57 +0100

    Creating test_api 3/3 passed

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,28 +1,31 @@
 import io
 import json
+from pathlib import Path 
 from ui.main import app
 
 def test_predict_valid_image():
     client = app.test_client()
-    with open("data/tests/Image.jpg", "rb") as f:
+    img_path = (Path(__file__).parent / ".." / "data" / "tests" / "Image.jpg").resolve() 
+    with open(img_path, "rb") as f:
         data = {"file": (io.BytesIO(f.read()), "Image.jpg")}
-        response = client.post("/api/predict", data = data, coontent_type = "multipart/form-data")
+        response = client.post("/api/predict", data=data, content_type="multipart/form-data")
 
-    assert response.status.cpde == 200
-    data = response.get_json()
-    assert "label" in data
-    assert "top_prob" in data
-    assert 0 <= data[top_prob] <= 1
+    print(response.status_code, response.data) 
+    assert response.status_code == 200
+    data_json = response.get_json() 
+    assert "label" in data_json
+    assert "top_prob" in data_json
+    assert 0 <= data_json["top_prob"] <= 1
 
 def test_predict_no_such_file():
     client = app.test_client()
     response = client.post("/api/predict")
-    assert response.status_code == 400 or "error" in response.get_json()
+    assert response.status_code == 400 or "error" in (response.get_json() or {})
 
 def test_predict_invalid_file_type():
-    client = app.test_clien()
+    client = app.test_client() 
     fake_file = io.BytesIO(b"not-an-image")
-    response = client.post("/api/predict", data = data, coontent_type = "multipart/form-data")
+    data = {"file": (fake_file, "not_image.txt")}  
+    response = client.post("/api/predict", data=data, content_type="multipart/form-data") 
     assert response.status_code >= 400
-
-
+    
